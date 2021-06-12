37f4f77928ad9fa5aaea6e4e5b7efb5e36bf61d7
suzuki pri <pg.econ4869@gmail.com>
2021-06-12 23:47:42 +0900

    feat: serverless

---
+++ iam_policy_document.tf
@@ -0,0 +1,29 @@
+# API Gateway Policy
+
+data "aws_iam_policy_document" "example_api_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["execute-api:Invoke"]
+    resources = ["arn:aws:execute-api:ap-northeast-1:*:*/*/*"]
+  }
+}
+
+# Lambda Policy
+
+data "aws_iam_policy_document" "example_lambda" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
---
+++ lambda.tf
@@ -0,0 +1,18 @@
+resource "aws_lambda_function" "example_api" {
+  filename      = "dummy_function.zip"
+  function_name = "example-api"
+  role          = aws_iam_role.example_lambda.arn
+  handler       = "lambda"
+  runtime       = "go1.x"
+
+  memory_size = 128
+  timeout     = 900
+}
+
+resource "aws_lambda_permission" "example_apigateway_lambda" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example_api.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.example_api.execution_arn}/*/*/*"
+}
